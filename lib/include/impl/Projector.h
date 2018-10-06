dd26dbf37e534e1c10b6b493bcbd4c3d4985d4be
Andrey L <green.anger@gmail.com>
2018-10-06 09:01:22 +0300

    Fixed Projector::projectInv # it had copy-pasted code, now it does what it has to + keeping projection center in projLon and projLat

--- lib/include/impl/Projector.h
+++ lib/include/impl/Projector.h
@@ -14,10 +14,15 @@ public:
 
     bool projectFwd( double lon, double lat, double& x, double& y ) const;
     bool projectInv( double x, double y, double& lon, double& lat ) const;
+    double projLon() const;
+    double projLat() const;
 
 private:
     PJ_CONTEXT* context_;       //!< PROJ4 Context
     PJ* projection_;            //!< PROJ4 projection
+
+    double projLon_;
+    double projLat_;
 };
 
 
--- lib/src/Projector.cpp
+++ lib/src/Projector.cpp
@@ -1,4 +1,5 @@
-﻿#include <cmath>
+﻿#include <iostream>
+#include <cmath>
 
 #include "Projector.h"
 
@@ -11,6 +12,8 @@ Projector::Projector()
 {
     context_ = proj_context_create();
     projection_ = proj_create( context_, "+proj=ortho +ellps=WGS84 +lon_0=0 +lat_0=0" );
+    projLon_ = 0.0;
+    projLat_ = 0.0;
 }
 
 
@@ -34,12 +37,24 @@ bool Projector::projectFwd( double lon, double lat, double& x, double& y ) const
 
 bool Projector::projectInv( double x, double y, double& lon, double& lat ) const
 {
-    PJ_COORD src = proj_coord( proj_torad( lon ), proj_torad( lat ), 0.0, 0.0 );
-    PJ_COORD dst = proj_trans( projection_, PJ_FWD, src );
-    x = dst.xy.x;
-    y = dst.xy.y;
+    PJ_COORD src = proj_coord( x, y, 0.0, 0.0 );
+    PJ_COORD dst = proj_trans( projection_, PJ_INV, src );
+    lon = proj_todeg( dst.lp.lam );
+    lat = proj_todeg( dst.lp.phi );
 
-    return ( x != HUGE_VAL && y != HUGE_VAL );
+    return ( lon != HUGE_VAL && lat != HUGE_VAL );
+}
+
+
+double Projector::projLon() const
+{
+    return projLon_;
+}
+
+
+double Projector::projLat() const
+{
+    return projLat_;
 }
 
 
